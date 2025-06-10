05ff5f08c96e3b91a25411d283aa90895f8fde0c
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-10 02:35:01 -0400

    add: curso de introduccion

---
+++ frontend/angular/src/app/pages/introduction/introduction.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import introductionMock from './introduccionMock.json';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-introduction',
+  imports: [MatButtonModule],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.scss',
+})
+export class IntroductionComponent {
+  intro = introductionMock;
+  initialSubject = 0;
+  totalSubjects = this.intro.length - 1;
+
+  title = this.intro[this.initialSubject].title;
+
+  gonext() {
+    this.initialSubject++;
+    this.update();
+  }
+
+  goback() {
+    this.initialSubject--;
+    this.update();
+  }
+
+  update() {
+    this.title = this.intro[this.initialSubject].title;
+  }
+}
