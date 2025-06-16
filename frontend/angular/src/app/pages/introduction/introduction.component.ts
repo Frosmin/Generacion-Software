cd07dd8b14ab86158e43f101725806aac7f8a527
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-16 15:02:08 -0400

    add: logic to introduction for search component to work

--- frontend/angular/src/app/pages/introduction/introduction.component.ts
+++ frontend/angular/src/app/pages/introduction/introduction.component.ts
@@ -10,26 +10,37 @@ import { SearchComponent } from '../../components/search/search.component';
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss',
 })
-
 export class IntroductionComponent {
-  curso: any = null;
+  resetCnt = 0;
+  curso: any = null;  
   intro = introductionMock;
   initialSubject = 0;
   totalSubjects = this.intro.length - 1;
-  salidaCodigo: string = '';
+  salidaCodigo = '';
   content = this.intro[this.initialSubject];
+
+  allCursos = this.intro.map((e) => {
+    return e.title;
+  });
 
   gonext() {
     this.initialSubject++;
+    this.resetCnt++;
     this.update();
   }
 
   goback() {
     this.initialSubject--;
+    this.resetCnt++;
     this.update();
   }
 
   update() {
     this.content = this.intro[this.initialSubject];
+  }
+
+  handleSelection(index: number) {
+    this.initialSubject = index;
+    this.update();
   }
 }
