dc3781b3dfff03d61a131074b7a65b1da54e1c37
Josuex123 <josue7pedro@gmail.com>
2025-06-25 01:54:50 -0400

    fix: pruebas unitarioas solucionado

--- frontend/angular/src/app/pages/create-course/create-course.component.spec.ts
+++ frontend/angular/src/app/pages/create-course/create-course.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CreateCourseComponent } from './create-course.component';
 
 describe('CreateCourseComponent', () => {
@@ -11,7 +12,7 @@ describe('CreateCourseComponent', () => {
     const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     await TestBed.configureTestingModule({
-      imports: [CreateCourseComponent],
+      imports: [HttpClientTestingModule,CreateCourseComponent],
       providers: [
         { provide: Router, useValue: routerSpy }
       ]
--- frontend/angular/src/app/pages/edit-course/edit-course.component.spec.ts
+++ frontend/angular/src/app/pages/edit-course/edit-course.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { EditCourseComponent } from './edit-course.component';
 
 describe('EditCourseComponent', () => {
@@ -18,7 +18,7 @@ describe('EditCourseComponent', () => {
     };
 
     await TestBed.configureTestingModule({
-      imports: [EditCourseComponent],
+      imports: [EditCourseComponent,HttpClientTestingModule],
       providers: [
         { provide: Router, useValue: routerSpy },
         { provide: ActivatedRoute, useValue: activatedRouteSpy }
